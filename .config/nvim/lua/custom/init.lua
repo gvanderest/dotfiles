389dd4ba47637a2512ada85f2ead70fc642f50ef
Guillaume VanderEst <guillest@amazon.com>
2024-05-28 13:27:57 -0700

    Update configs

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -1,8 +1,9 @@
 if vim.g.neovide then
     vim.o.guifont = "FiraCode Nerd Font:h11"
-    vim.opt.linespace = 4
+    vim.opt.linespace = 0
 end
 
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.tabstop = 4
+
